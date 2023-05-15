64e2095aef156ba26d3a7df802ae13403efebdaa
indrakj <indrakomara13@gmail.com>
2023-05-15 13:48:20 +0700

    shopping list app

---
+++ src/components/Empty/index.js
@@ -0,0 +1,11 @@
+import styles from './Empty.module.css'
+
+const Empty = () => {
+    return (
+        <div className={styles.Empty}>
+            Daftar Masih Kosong
+        </div>
+    )
+}
+
+export default Empty
